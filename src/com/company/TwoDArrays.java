0fe9adb35468ef36bd8d0c717e3b84b9c3a98587
rileybr <rileybr@ssfs.org>
2020-03-20 14:16:25 -0400

    checks if 2d array is a square

--- src/com/company/TwoDArrays.java
+++ src/com/company/TwoDArrays.java
@@ -21,8 +21,20 @@ public class TwoDArrays {
         }
         return total;
     }
+
+    public static boolean isSquare(int[][] list){
+        if (list.length == list[1].length){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
     public static void main(String[] args){
-        int[][] arr = {{1,2,3}, {4,5,6}, {7,8,9}};
-        System.out.println(sumAll(arr));
+        int[][] arr1 = {{1, 2, 3, 4}, {6, 2, 5, 3}, {9, 0, 1, 7}, {3, 1, 8, 6}};
+        int[][] arr2 = {{1, 6, 4}, {6, 2, 1}};
+        System.out.println(isSquare(arr1));
+        System.out.println(isSquare(arr2));
     }
 }
